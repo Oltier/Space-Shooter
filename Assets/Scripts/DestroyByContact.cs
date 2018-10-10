8f0f191bc87803741e84725dbbdcb63586f6298f
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-10 20:43:42 +0300

    add destroy functionality to asteroid on collision with shoots

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Boundary")) return;
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
