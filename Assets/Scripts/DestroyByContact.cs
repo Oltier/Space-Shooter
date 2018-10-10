9d2a0e9385d6e7bb818a78f7ba02005f0aaf9ea0
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-10 20:51:04 +0300

    add explosiooonnnssgit add -Agit add -A

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using UnityEngine;
 
-public class DestroyByContact : MonoBehaviour {
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject AsteroidExplosion;
+	public GameObject PlayerExplosion;
+	
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.CompareTag("Boundary")) return;
 		Destroy(other.gameObject);
 		Destroy(gameObject);
+		Instantiate(AsteroidExplosion, transform.position, transform.rotation);
+		Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
 	}
 }
