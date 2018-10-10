afa9e0f15ed9190dd08ae55655732be18a36cf20
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-10 18:08:00 +0300

    add movement script and boundaries to player

---
+++ Assets/Scripts/MyPlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float XMin, XMax, ZMin, ZMax;
+}
+
+public class MyPlayerController : MonoBehaviour
+{
+    public float Speed;
+    public Boundary Boundary;
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = movement * Speed;
+
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(rb.position.x, Boundary.XMin, Boundary.XMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, Boundary.ZMin, Boundary.ZMax)
+        );
+    }
+}
